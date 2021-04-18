0c9ba5917e6eb2e7c1d55da57bbcfe3f8ab3dfd5
tomi-mercado <tmercadoslp@gmail.com>
2021-04-18 10:49:57 -0300

    (index): use channelIds to get masterchefId

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 import dotenv from 'dotenv';
 
 import openLastProgram from './functions/openLastProgram.js';
+import { masterChefId } from './channelIds.js';
 
 dotenv.config();
-
-const masterChefId = 'UCsf7o62bd1t0zy8csOPm1xg';
 
 openLastProgram(masterChefId, ['Programa', 'MasterChef Argentina 2021'])
